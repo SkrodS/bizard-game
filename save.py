b0de5d44d9bc8277642da94efc546c3069676016
SkrodS <r.widjeback@hotmail.se>
2021-05-20 14:40:17 +0200

    test

--- save.py
+++ save.py
@@ -1,5 +1,4 @@
 import json
-import bcrypt
 from datetime import datetime
 import os
 
@@ -7,17 +6,27 @@ def save(wave, difficulty):
     data = {}
     data['difficulty'] = difficulty
     data['wave'] = wave
-    data_string = json.dumps(data).encode('UTF-8')
-    hashed = bcrypt.hashpw(data_string, bcrypt.gensalt())
+    data_string = json.dumps(data)
     date = datetime.today().strftime('%Y-%m-%d-%H:%M')
+    difficulty_string = ''
+
+    if difficulty == 2:
+        difficulty_string = 'easy'
+    elif difficulty == 4:
+        difficulty_string = 'medium'
+    elif difficulty == 6:
+        difficulty_string = 'hard'
 
     index = ''
+
+    print(data_string)
+    print(date)
     while True:
         try:
             if not os.path.exists('save_files'):
                 os.makedirs('save_files')
-            with open(f'save_files/save {date}'+index, 'x') as outfile:
-                print(hashed, file=outfile)
+            with open(f'save_files/{difficulty_string} wave {wave} ({date})'+index, 'x') as outfile:
+                print(data_string, file=outfile)
             break
         except IOError:
             if index:
